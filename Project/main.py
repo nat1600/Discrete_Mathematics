1628f0d56b94ae9410e64f7b1a91b97a6901b1fa
Nataly Sierra <ksierram@unal.edu.co>
2024-06-29 18:54:32 -0500

    Add is_prime function

---
+++ Project/main.py
@@ -0,0 +1,16 @@
+import random
+import math
+
+def is_prime(number):
+    '''
+    Check if a number is prime
+    :param number: Integer to check if is prime
+    :return: True if the number is prime, False if it is not
+    .
+    '''
+    if number < 2:
+        return False
+    for i in range(2, int(math.sqrt(number)) + 1):
+        if number % i == 0:
+            return False
+        return True
