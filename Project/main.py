c2bba6a604e3a9d9a03d44ab278f2b6ca8584d40
Nataly Sierra <ksierram@unal.edu.co>
2024-07-04 12:45:13 -0500

    Add  functions for RSA key generation and utility functions

--- Project/main.py
+++ Project/main.py
@@ -26,3 +26,56 @@ def generate_prime(min_value, max_value):
     while not is_prime(prime):
         prime = random.randint(min_value, max_value)
     return prime
+
+def extended_gcd(a, b):
+    '''
+       the extended Euclidean algorithm
+
+       :param a: The first integer
+       :param b: The second integer
+       :return: A tuple where g is the greatest common divisor
+       '''
+    if a == 0:
+        return (b, 0, 1)
+    else:
+        g, y, x = extended_gcd(b % a, a)
+        return (g, x - (b // a) * y, y)
+
+def mod_inverse(e, phi):
+    '''
+    the modular multiplicative inverse of e modulo phi.
+    :param e: integer whose modular inverse is to be calculated
+    :param phi: modulus
+    :return:  modular multiplicative inverse of e modulo phi
+    '''
+    g, x, y = extended_gcd(e, phi)
+    if g != 1:
+        raise Exception('modular inverse does not exist')
+    else:
+        return x % phi
+
+
+def generate_keys():
+    p,q = generate_prime(1000,5000), generate_prime(1000,5000)
+
+    while p == q:
+        q = generate_prime(1000,5000)
+
+    n = p * q
+    phi_n = (p - 1) * (q - 1)
+
+    e = random.randint(3, phi_n - 1)
+
+    while g != 1:
+        e = random.randint(3, phi_n - 1)
+        g, _, _ = extended_gcd(e, phi_n)
+
+    d = mod_inverse(e, phi_n)
+
+    return (e, n), (d, n), p, q, phi_n
+
+
+
+
+
+
